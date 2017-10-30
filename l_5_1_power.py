684d0f182d2987b4ef43c7562d089b337f9fa083
duhe <lucciv@163.com>
2017-10-30 09:14:15 +0800

    revise l_5_1_power python file - input

--- l_5_1_power.py
+++ l_5_1_power.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #L5_1 函数
-x=6
-n=3
 def power(x,n=2):
     s=1
     while n>0:
         n=n-1
         s=s*x
     return s
+
+x=int(input(please input your number: x= ))
+n=int(input(please input your number: n= ))
 print(power(x,n))
