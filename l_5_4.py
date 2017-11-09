344867a58ce9afe4f53a8f3d7ac7ef582eaad50c
duhe <lucciv@163.com>
2017-11-09 10:42:54 +0800

    add l_5_4 python file

---
+++ l_5_4.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#L5_3
+
+# function1
+def function1():
+   pass
+# function2
+def function2():
+   pass
+# function3
+def function3():
+   pass
+# function4
+def function4():
+   pass
+
+# x=int(input(""))
+# n=int(input(""))
+# print()
