6f19eeb2b70a4cf40952ace8e559e89dbe5995d0
duhe <lucciv@163.com>
2017-11-13 08:57:25 +0800

    wrote quadratic funcition

--- l_5_2_quadratic.py
+++ l_5_2_quadratic.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #L5_2_二元一次方程
+import math
 
-def myquadratic():
-   pass
+def myquadratic(a,b,c):
+   ader=b**2-4*a*c
+   if ader>0:
+      x1=(-b+math.sqrt(ader))/(2*a)
+      x2=(-b-math.sqrt(ader))/(2*a)
+      return x1,x2
+   elif ader==0:
+      x1=x2=-b/2*a
+      return x1,x2
+   elif a==0:
+      x=-c/b
+      return x
+   else:
+      return('此方程无解')
 
-x=int(input("Please input x: "))
-y=int(input("Please input y: "))
-z=int(input("Please input z: "))
-print(myquadratic(x,y,z))
+a=int(input("Please input a: "))
+b=int(input("Please input b: "))
+c=int(input("Please input c: "))
+
+print(myquadratic(a,b,c))
