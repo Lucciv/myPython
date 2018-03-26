d1120135e1f2682374cdc7664884b6ee31826d9e
duhe <lucciv@163.com>
2018-03-26 11:17:54 +0800

    Add and Rename Python file 1_6_4_yktriangles

---
+++ l_6_4_yktriangles.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def triangles():
+    lst = [1]
+    count = 0
+    while (count < 10):
+        yield lst
+        lst = [1] + [(lst[i - 1] + lst[i]) for i in range(1, len(lst))] + [1]
+        count += 1
+
+# 测试
+n = 0
+results = []
+for t in triangles():
+    print(t)
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+if results == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+    [1, 7, 21, 35, 35, 21, 7, 1],
+    [1, 8, 28, 56, 70, 56, 28, 8, 1],
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+]:
+    print('测试通过!')
+else:
+    print('测试失败!')
